6b90e928dbb5e1d038f0f93b78cc5ce1ea2af2bc
nesya04061919 <34899414+nesya04061919@users.noreply.github.com>
2018-02-05 20:19:23 -0700

    read and write a file

---
+++ practice/input_output/ex_1.py
@@ -0,0 +1,4 @@
+my_file = open("output.txt", "r+")
+
+# This told Python to open output.txt in "r+" mode (stands for read and write).
+# Then it stored the result of this operation in a file object, my_file.
