a374e732d31accc2c53ed1736863da061fa22941
Emily Schroeder <esch256496@gmail.com>
2018-04-15 14:46:26 -0600

    wip WORKING

--- wordnik-repl/game.py
+++ wordnik-repl/game.py
@@ -31,7 +31,7 @@ class Game(object):
         """set up game"""
         self.words = pick_ten(game_dictionary)
         self.distract = other_defs(self.words, game_dictionary)
-        self.score = 0
+        self.score = 0.0
 
     def play(self):
         """Actually play the game."""
@@ -39,9 +39,24 @@ class Game(object):
             # this is the round
             for w in self.words:
                 # this is the play within the round
+                all_defs = random.sample(self.distract, 3)
+                correct = self.words[w]
+                all_defs.append(correct)
+                random.shuffle(all_defs)
                 print('the word is: {}'.format(w))
+                print('\n\nthe definitions are:')
+                for i in range(len(all_defs)):
+                    print('\n     {} -- {}'.format(i, all_defs[i]))
                 answer = input('Guess please: ').strip().lower()
-                
+                if all_defs[int(answer)] == correct:
+                    print('Correct!\n')
+                    print('{} -- {}\n\n\n'.format(w, correct))
+                    self.score += 0.5
+                else:
+                    print('Incorrect :(\n')
+                    print('the correct definition for {} is: {}\n\n\n'.format(w, correct))
+        print('Final score: {} out of 10'.format(self.score))
+
 # def session_words(master_dictionary):
 #  """this function selects the 10 words for each session"""
 #  master_dictionary = self.master_dict
