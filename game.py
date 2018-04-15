a4d9637aa3558af63edd351422534231085a38e1
Emily Schroeder <esch256496@gmail.com>
2018-04-15 11:36:52 -0600

    wip game

---
+++ game.py
@@ -0,0 +1,34 @@
+def session_words(master_dictionary):
+  """this function selects the 10 words for each session"""
+  master_dictionary = self.master_dict
+  session_words = {}
+  # example of how to add to dict...
+  # session_words['veneration'] = 'condition or status of one who is venerated'
+
+  """then prints those 10 words and their definitions"""
+   for word in session_words:
+     print(word, ':',  session_words[word])
+
+def play_game(self):
+  """plays both rounds of the game"""
+  for word in word_list:
+    def give_question(self):
+      """prints question and four answers for player"""
+
+
+    def select_answer(self);
+      """allows the player to select answer, determins if correct"""
+
+
+    def question_response(self):
+      """prints question and answer, identify users choice"""
+
+
+    def assign_points(self):
+      """assigns new point value to each word"""
+
+
+    def second_chance(self):
+      """re-shows the questions answered wrong"""
+
+
